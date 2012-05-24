6fd5fe86bfa1ae791e39198f79be037bd6243c06
Jorge Mayo <lookmmm@gmail.com>
2012-05-24 12:57:05 -0500

    Comenzando el Proyecto: php,css,js

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Baripedia.pe</title>
+</head>
+<body>
+	<div>
+	<div id="logo">Baripedia.pe</div>
+	</div>
+	<div id="help">
+	<ul class="menu_inline">
+		<a href="sign_up.php"><li>Como Afiliarse?</li></a>
+		<a href="#"><li>Ayuda</li></a>
+	</ul>
+	</div>
+	<div id="content">
+	<input id="search" type="text" name="searched" />
+	</div>
+	<div id="shotwell">
+	<ul class="slide"></ul>
+	<li>
+		<img src="img/1.png" alt="La Noche" />
+	</li>
+	<li>
+		<img src="img/1.png" alt="La Noche" />
+	</li>
+	<li>
+		<img src="img/1.png" alt="La Noche" />
+	</li>
+	</div>
+	<div id="footer">blablabla</div>
+</body>
+</html>
+
